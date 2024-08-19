e4077ebaa90fc8047033148eb77100989dca5f58
Maciek <maciek.alencynowicz@gmail.com>
2024-08-19 16:42:29 +0200

    Initial commit with backend structure and PostgreSQL connection setup

---
+++ backend/src/config/dbConnection.js
@@ -0,0 +1,20 @@
+import pg from "pg";
+import env from "dotenv";
+
+env.config();
+
+const db = new pg.Client({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+});
+
+db.connect().then(() => {
+    console.log(`Connected to the database on port ${process.env.PG_PORT}`);
+}).catch(err => {
+    console.log(`Connection error: ${err}`);
+})
+
+export default db 
