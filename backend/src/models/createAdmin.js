69dbfc8125839559a3f491e663fdabb1d834c572
Maciek <maciek.alencynowicz@gmail.com>
2024-09-15 19:52:34 +0200

    Enhance admin creation process

--- backend/src/models/createAdmin.js
+++ backend/src/models/createAdmin.js
@@ -1,21 +1,36 @@
 import db from "../config/dbConnection.js";
 import bcrypt from "bcrypt";
 import { registerUserTransaction } from "./userModel.js";
+import { generatePassword } from "../utils/generators.js";
+import { hashPassword } from "../utils/hashing.js";
+import chalk from "chalk";
 
-const saltRounds = 12;
-const passwordLength = 10;
-
-const generatePassword = (passwordLength) => {
-  const characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
-  let password = "";
-  for (let i = 0; i < passwordLength; i++) {
-    const randomIndex = Math.floor(Math.random() * characters.length);
-    password += characters[randomIndex];
-  }
-  return password;
-};
-
+/**
+ * Creates a new admin in the system by inserting data into the users and admins tables
+ * 
+ * This function creates a new admin with the given data, generates a password,
+ * hashes it, and saves the information in the database
+ * 
+ * @param {string} name - admin's first name
+ * @param {string} surname - admin's last name
+ * @param {string} pesel - admin's PESEL number
+ * @param {string} dialingCode - dialing code for the admin's phone number
+ * @param {string} phoneNumber - admin's phone number
+ * @param {string} email - admin's email address
+ * 
+ * @returns {void} This function does not return a value. It logs the result to the console and exits the process
+ * 
+ * @example
+ * // Running the script from the /backend/src/models folder
+ * // Command to create an admin:
+ * node createAdmin.js "Name" "Lastname" "12345678901" "+48" "111222333" "admin.test@gmail.com"
+ * 
+ * // Expected console output on success:
+ * // Admin successfully created, password: <generated_password>
+ * 
+ * // Expected console output on failure:
+ * // Error adding admin to the database <error_message>
+ */
 const createAdmin = async (
   name,
   surname,
@@ -24,11 +39,12 @@ const createAdmin = async (
   phoneNumber,
   email
 ) => {
+  const client = await db.connect();
   try {
-    await db.query("BEGIN")
+    await client.query("BEGIN")
 
-    const password = generatePassword(passwordLength);
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const password = generatePassword();
+    const hashedPassword = await hashPassword(password);
 
     const data = {
       name,
@@ -39,25 +55,26 @@ const createAdmin = async (
       email,
       password: hashedPassword,
     };
-    const userId = await registerUserTransaction(data, "admin");
+    const userId = await registerUserTransaction(data, "admin", client);
     console.log(userId);
 
-    //userId is > 0 if successful and -1 if not
     if(userId === -1){
       throw new error("error creating user(admin) in the users table")
     }
 
     const query = `INSERT INTO admins (user_id, must_change_password) 
                         VALUES($1, $2)`;
-    await db.query(query, [userId, true]);
+    await client.query(query, [userId, true]);
 
-    await db.query("COMMIT")    
-    console.log(`Admin sucessfully created, password: ${password}`);
+    await client.query("COMMIT")    
+    console.log(`Admin sucessfully created, password: ${chalk.yellow(password)}`);
     process.exit(0)
   } catch (error) {
-    await db.query("ROLLBACK")
+    await client.query("ROLLBACK")
     console.log("Error adding admin to the database", error);
     process.exit(1)
+  }finally{
+    client.release()
   }
 };
 
